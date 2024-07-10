08089eeffc9b442da1c7343a70bf66c6de1a72c9
kakaosdk <kakaosdk.dev@kakaocorp.com>
2024-07-10 10:57:37 +0900

    2.22.4

--- Sources/KakaoSDKFriendCore/KakaoSDKFriendCore.xcframework/ios-arm64_x86_64-simulator/KakaoSDKFriendCore.framework/Headers/KakaoSDKFriendCore-Swift.h
+++ Sources/KakaoSDKFriendCore/KakaoSDKFriendCore.xcframework/ios-arm64_x86_64-simulator/KakaoSDKFriendCore.framework/Headers/KakaoSDKFriendCore-Swift.h
@@ -301,7 +301,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 
 
 
-
 #endif
 #if __has_attribute(external_source_symbol)
 # pragma clang attribute pop
@@ -609,7 +608,6 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #endif
 
 #if defined(__OBJC__)
-
 
 
 
